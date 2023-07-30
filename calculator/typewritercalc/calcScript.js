d776e3889499bca871c3ef499fc39e13caa45c12
Tiffany Chen <tchen556@gmail.com>
2023-07-29 20:15:01 -0400

    fixed calc bug, clear the values when clicking a num after computing

--- calculator/typewritercalc/calcScript.js
+++ calculator/typewritercalc/calcScript.js
@@ -12,6 +12,9 @@ function doCalc(val){
     if(val == '0' || val == '1' || val == '2' || val == '3' || val == '4' || val == '5' || val == '6' ||
         val == '7' || val == '8' ||val == '9' ){
         if(!op){
+            if(sum > 0){
+                doClear();
+            }
             if(num1){
                 num1 += val;
             }
